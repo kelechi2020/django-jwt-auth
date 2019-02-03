0be21e023975272b5681d628b0c263b813ca7397
KELECHI <egbosikelechi@gmail.com>
2019-02-03 18:21:31 +0100

    Add registration views

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 
-from music.views import ListSongsView, LoginView
+from music.views import ListSongsView, LoginView, RegisterUsers
 
 urlpatterns = [
     path('songs/', ListSongsView.as_view(), name="songs-all"),
     path('api-token-auth/', obtain_jwt_token, name='create-token'),
-    path('auth/login/', LoginView.as_view(), name="auth-login")
+    path('auth/login/', LoginView.as_view(), name="auth-login"),
+    path('auth/register/', RegisterUsers.as_view(), name="auth-register")
 ]
